98d5700e45ad524f2fa55f0671c23a557f363f91
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2026-02-13 21:20:10 +0530

    debug

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -16,6 +16,8 @@ export default function Signup() {
 
         try {
             const { data } = await api.post("/register", form);
+            console.log(data);
+
             toast.success(data.message);
             navigate("/login");
         } catch (err) {
