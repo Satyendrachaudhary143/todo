ee247cc5442f250a7aa71b1ce613fa26768ebfcd
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2026-02-13 21:16:18 +0530

    backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://todo-at57.vercel.app",
     withCredentials: true, // IMPORTANT for cookies
 });
 
